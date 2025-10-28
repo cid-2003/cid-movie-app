4245970b2a9f842073ba192d0c458ea6bbce8a71
Cédrick <cedrickemmanuel69@gmail.com>
2025-10-28 23:45:55 +0100

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -6,13 +6,14 @@ import { BrowserRouter, Route } from "react-router-dom";
 import Header from "./components/header/Header";
 import Footer from "./components/footer/Footer";
 import AppRoutes from "./routes/Routes";
+import Home from "./pages/Home";
 
 function App() {
   return (
     <BrowserRouter>
       <Header />
 
-      <AppRoutes />
+      <Home />
 
       <Footer />
     </BrowserRouter>
